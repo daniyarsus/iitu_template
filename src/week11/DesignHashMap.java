27d73d1c780be08d1d8a31523455bdc19490a04c
daniyarsus <daniyar.kanu@mail.ru>
2024-11-18 13:56:37 +0500

    Added 11 week.

---
+++ src/week11/DesignHashMap.java
@@ -0,0 +1,24 @@
+package week11;
+
+import java.util.Arrays;
+
+class DesignHashMap {
+    private final int[] map;
+
+    public DesignHashMap() {
+        map = new int[1000001];
+        Arrays.fill(map, -1);
+    }
+
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+
+    public int get(int key) {
+        return map[key];
+    }
+
+    public void remove(int key) {
+        map[key] = -1;
+    }
+}
